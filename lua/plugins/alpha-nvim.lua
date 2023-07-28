77cfdcfaa7376f2c68e89dc1a869dad81eb1e901
zhaohongxuan <hxzhenu@gmail.com>
2023-07-28 13:20:17 +0800

    feat: update alpha

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -16,10 +16,11 @@ dashboard.section.header.val = {
 -- Set menu
 dashboard.section.buttons.val = {
     dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f", "  > Find file", ":cd $HOME/Developer/VSCodeProjects/ | Telescope find_files<CR>"),
+    dashboard.button( "f", "󰈞  > Find file", ":cd $HOME/Developer/VSCodeProjects/ | Telescope find_files<CR>"),
     dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "p", "  > Project"  , ":lua require'telescope'.extensions.project.project{}<CR>"),
     dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+    dashboard.button( "q", "󰗼  > Quit NVIM", ":qa<CR>"),
 }
 
 -- Set footer
